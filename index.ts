21d8c89f24c6864cc50dbb085c9aec4fe59e8f01
islam-shariful <imdshariful171@gmail.com>
2025-08-30 01:34:15 +0600

    Ran client & server app together

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+  { command: "bun run dev", cwd: "packages/client", name: "client", prefixColor: "green" },
+  { command: "bun run dev", cwd: "packages/server", name: "server", prefixColor: "cyan" }
+]);
