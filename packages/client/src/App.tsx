b85c6c2af46f215057c478f38444ab56d0ff129b
islam-shariful <imdshariful171@gmail.com>
2025-08-30 12:52:28 +0600

    Setup Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <div className="p-4">
          <h1>Client Application</h1>
          <p className="font-bold text-3xl">{message || 'Loading message...'}</p>
-         <Button variant="outline">Click Me</Button>
+         <Button variant="outline">Click Me!</Button>
       </div>
    );
 }
