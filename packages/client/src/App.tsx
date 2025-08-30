5887f58dff2aff6a5a8f8f40069ddfad1cd08f19
islam-shariful <imdshariful171@gmail.com>
2025-08-30 11:48:43 +0600

    Setup tailwind CSS

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <h1>Client Application</h1>
-      <p>{message || 'Loading message...'}</p>
+      <p className='font-bold p-4 text-3xl'>{message || 'Loading message...'}</p>
     </div>
   );
 }
